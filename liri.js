2bc0fd0ce405762efcb6e3dd703840256404b4e6
Meanderthal00 <meanderthal00@yahoo.com>
2018-01-13 08:21:06 -0500

    working on the OMDB portion

--- liri.js
+++ liri.js
@@ -60,63 +60,6 @@ function twitter() {
         }
     });
 }
-
-function spotify() {
-    var Spotify = require("node-spotify-api");
-    var nodeInput = process.argv;
-    var client = new Spotify(keys.spotify);
-
-    for (var i = 0; i < nodeInput.length; i++) {
-        if (i > 3 && i < nodeInput.length) {
-            songTitle = songTitle + " " + nodeInput[i];
-        } else {
-            songTitle += nodeInput[i]
-        }
-        // console.log(nodeInput);
-        // console.log("-------------------");
-
-    }
-
-
-
-    client.search({
-            type: "track",
-            query: nodeInput
-        },
-        function (error, data) {
-            if (error) {
-                return console.log("Oops, Error: " + error);
-            } else if (error, data) {
-                console.log(data);
-                console.log("====================");
-                console.log("Song Name is: ", nodeInput);
-                console.log("Artist is: ", data.tracks.items [0].album.artist[0].name);
-
-            }
-        });
-
-}
-
-
-//     if (input != undefined) {
-//         client.search({
-//                 type: "track",
-//                 query: input
-//             }
-//             else(function (data) {
-//                     console.log(data.body.tracks.items[0].artists[0].name);
-//                 },
-//                 function (error) {
-//                     console.log(error);
-
-//                 }));
-//     }
-// }
-
-
-
-
-
 // *node liri.js spotify-this-song "<song name here>"* this command logs the following info to bash: 
 // Artist(s)
 // The song's name
@@ -125,6 +68,45 @@ function spotify() {
 // If no song is provided, then it defaults to "The Sign" by Ace of Base
 // function spotify(){
 //     var spotify = require("node-spotify-api");
+
+function spotify() {
+    var Spotify = require("node-spotify-api");
+    var songInput = process.argv;
+    var client = new Spotify(keys.spotify);
+
+    for (var i = 0; i < songInput.length; i++) {
+        if (i > 3 && i < songInput.length) {
+            songTitle = songTitle + " " + songInput[i];
+        } else {
+            songTitle += songInput[i]
+        }
+        // console.log(songInput);
+        // console.log("-------------------");
+
+    }
+
+
+    client.search({
+            type: "track",
+            query: songInput
+        },
+        function (error, data) {
+            if (error) {
+                return console.log("Oops, Error: " + error);
+            } else if (error, data) {
+                console.log(data);
+                console.log("====================");
+                console.log("Song Name is: ", songInput);
+                console.log("Artist is: ", data.tracks.items [0].album.artist[0].name);
+                console.log("Album Name is: ",data.tracks.items[0].album.name);
+                console.log("Preview: ",data.tracks.items[0].preview_url);
+                console.log("====================");
+
+            }
+        });
+
+}
+
 
 
 
@@ -139,6 +121,45 @@ function spotify() {
 // Plot synopsis
 // Actors in the movie
 // If no movie title is supplied, liri defaults to "Mr.Nobody"
+
+function movie() {
+    var request = require("request");
+    var movieInput = process.argv;
+    
+
+
+    for (var i = 3; i < movieInput.length; i++){
+        if (i >3 && i <movieInput.length) {
+            movieTitle = movieTitle + "+" + movieInput[i];
+        }
+        else{
+            movieTitle += movieTitle[i];
+        }
+    }
+    
+    
+    var queryUrlMovie = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+
+    request(queryUrlMovie, function(error, response, body){
+        if (!error && response.statusCode === 200) {
+            // console.log(response);
+            console.log("You typed: "+ movieInput);
+           console.log("====================");
+           console.log("Title: " + JSON.parse(body).Title);
+           console.log("Year: " + JSON.parse(body).Year);
+           console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+        //    console.log("Rotten Tomatoes Rating: "+ JSON.parse(body).Ratings[0].Value);
+           console.log("Country: " + JSON.parse(body).Country);
+           console.log("Language: " + JSON.parse(body).Language);
+           console.log("Plot: " + JSON.parse(body).Plot);
+           console.log("Actors: " + JSON.parse(body).Actors);
+           console.log("====================");            
+        }
+        else {
+            console.log("Uh-oh, There was a problem, try again.");
+        }
+    });
+}
 
 // *node liri do-what-it-says*
 // using 'fs' Node package, liri will take the text inside of random.txt and use it to call a command
